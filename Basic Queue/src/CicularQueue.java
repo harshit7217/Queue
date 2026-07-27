4349ddd537a047632d585253669af7974a65d026
Prasang41 <harshitchaudhary568@gmail.com>
2026-07-27 10:17:59 +0530

    Initial commit

---
+++ Basic Queue/src/CicularQueue.java
@@ -0,0 +1,78 @@
+public class CicularQueue {
+
+    static class Queue {
+        static int arr[];
+        static int size;
+        static  int rear = -1;
+        static int front = -1;
+
+        Queue(int n){
+            arr= new int[n];
+            this.size = n;
+        }
+
+        public static  boolean isEmpty() {
+            return rear == -1 && front == -1;
+        }
+
+        public static boolean isFull() {
+            return (rear+1)%size == front;
+        }
+
+//        enqueue
+        public static void add(int data){
+            if(isFull()){
+                System.out.println("Full queue");
+                return;
+            }
+            if(front == -1){
+                front = 0;
+            }
+            rear = (rear+1)%size;
+            arr[rear] = data;
+        }
+
+//        dequeue
+        public static int remove() {
+            if(isEmpty()){
+                System.out.println("Queue Empty");
+                return -1;
+            }
+
+            int result = arr[front];
+            if(rear == front){
+                rear = -1;
+                front = -1;
+            }else {
+                front = (front+1)%size;
+            }
+            return arr[front];
+        }
+
+        public static int peek() {
+            if(isEmpty()){
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            return arr[front];
+        }
+
+    }
+
+    public static void main(String[] args){
+        Main.Queue q = new Main.Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println("First remove "+q.remove());
+        q.add(6);
+        q.add(7);
+//        1,2,3
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
